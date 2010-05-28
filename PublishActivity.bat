2be0cb78ac857312a636de05cbe3f7874cd80033
Gary Sherman <gsherman@gmail.com>
2010-05-28 16:11:06 -0500

    updated readme

---
+++ PublishActivity.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Directory where the powershell script is located:
+SET WORKING_DIR=C:\dovetail\powershell
+
+cd %WORKING_DIR%
+
+SET POWERSHELL=%SystemRoot%\SYSTEM32\WindowsPowerShell\v1.0\powershell.exe
+
+%POWERSHELL% ./PublishActivity.ps1 %*
